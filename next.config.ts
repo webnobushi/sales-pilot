b74df48e73e9b796c66126ab6b501a5f6edbb8f6
kishimoto ryoichi <kishimoto@qualita-lab.com>
2025-08-25 18:44:36 +0900

    first commit

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
